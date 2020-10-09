c659b8cad1e353f67f290e734c5c732cf1a21d32
Don Marti <dmarti@zgp.org>
2020-10-09 16:54:37 -0700

    Use primary* columns from user data export

--- tools/extract-csv.py
+++ tools/extract-csv.py
@@ -10,13 +10,13 @@ for pathname in sys.argv[1:]:
     with open(sys.argv[1]) as cfile:
         reader = csv.DictReader(cfile)
         for row in reader:
-            addressblock = row['user_submitted_address1']
-            two = row.get('user_submitted_address2')
+            addressblock = row['primary_address1']
+            two = row.get('primary_address2')
             if two:
                 addressblock += ('<br>%s' % two)
-            addressblock += ('<br>%s, %s %s' % (row['user_submitted_city'],
-                                                   row['user_submitted_state'],
-                                                   row['user_submitted_zip']))
+            addressblock += ('<br>%s, %s %s' % (row['primary_city'],
+                                                   row['primary_state'],
+                                                   row['primary_zip']))
             environ = ' '.join(["date='%s'" % datetime.now().strftime("%B %d, %Y"),
                        "name='%s'" % row['full_name'],
                        "email='%s'" % row['email'],
