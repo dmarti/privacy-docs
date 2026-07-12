2b13e1eccfcaa46f65fc05ed7a940a412d5ed57f
Don Marti <dmarti@zgp.org>
2026-07-12 16:16:47 -0700

    WIP

--- tools/jex.py
+++ tools/jex.py
@@ -14,6 +14,7 @@ from dataclasses import dataclass, field
 import tldextract
 import json
 from pprint import pp
+import re
 import sys
 from urllib.parse import parse_qs, urlparse, urlunparse
 
@@ -30,6 +31,32 @@ def index_tracker(t):
         trackers_by_value[v].append(t)
     except KeyError:
         trackers_by_value[v] = [t]
+
+def error(s):
+    print(s, file=sys.stdout)
+
+def possible_hex_codes(stuff):
+    hexstr = r'\b[0-9a-fA-F]+\b'
+    result = []
+    for found in re.findall(hexstr, stuff):
+        if len(found) <= 16:
+            continue
+        if (result != stuff) and not (found in result):
+            result.append(found)
+    return result
+
+def possible_uuids(stuff):
+    uuidstr = r'\b[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\b'
+    result = []
+    for found in re.findall(uuidstr, stuff):
+        if not found in result:
+            result.append(found)
+    return result
+
+def possible_codes(stuff):
+    all_codes = possible_hex_codes(stuff) + possible_uuids(stuff)
+    print(all_codes)
+    return all_codes
 
 @dataclass
 class Tracker:
@@ -56,7 +83,12 @@ print("Site is %s and first party domain is %s" % (top_fqdn, first_party))
 for e in l.get('entries'):
     req = e.get('request')
     res = e.get('response')
-    del(e['_initiator'])
+    url = req.get('url')
+
+    try:
+        del(e['_initiator'])
+    except KeyError:
+        pass
 
     for q in req.get('queryString'):
         rc = Tracker(ttype='queryString', key=q.get('name'), value=q.get('value'), url = req.get('url'))
@@ -65,10 +97,26 @@ for e in l.get('entries'):
     for rcookie in req.get('cookies'):
         rc = Tracker(ttype='request_cookie', key=rcookie.get('name'), value=rcookie.get('value'), url = rcookie.get('domain'))
         index_tracker(rc)
+        for code in possible_codes(rcookie.get('value')):
+            rc = Tracker(ttype='request_cookie_fragment', key=rcookie.get('name'), value=code, url = rcookie.get('domain'))
+            index_tracker(rc)
 
     for rcookie in res.get('cookies'):
         rc = Tracker(ttype='request_cookie', key=rcookie.get('name'), value=rcookie.get('value'), url = rcookie.get('domain'))
         index_tracker(rc)
+
+    con = res.get('content')
+    mime = con.get('mimeType')
+    #if mime == 'application/json':
+    if mime and not mime.startswith('image/'):
+        text = con.get('text')
+        size = con.get('size')
+        for possible in possible_codes(text):
+            print(" ---> ", possible)
+            rc = Tracker(ttype="hex_response", key=con, value=possible, url = req.get('url'))
+
+        if size > 0 and not text:
+            error("%s body of %d bytes missing from %s" % (mime, size, req.get('url')))
 
     u = req.get('url')
     ex = tldextract.extract(u)
